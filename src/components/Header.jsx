bab070ab434ef57c760eeee3b8f4c3201584306f
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-15 16:32:40 -0500

    componente header

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import img from '../assets/logo-imax.png';
+import { Link } from 'react-router-dom';
+
+const header = () => (
+    <section className="home">
+        <header>
+            <img className="logo-imax" src={img}  alt="logo"/>
+            <nav className= "menu">
+                <ul className="menubar">
+                <li>
+                    <Link  to="/perfil">Perfil</Link>
+                </li>
+                <li>
+                    <Link  to="/home">Bienes Futuros</Link>
+                    <ul>       
+                        <li> <Link  to="/proyectos">Proyectos</Link></li>
+                        <li> <Link  to="/nuevo">Nuevo Proyectos</Link></li>     
+                    </ul>
+                </li>
+                <li>
+                <a className="logOut">Cerrar Sesion <i className="fas fa-sign-out-alt"/></a>
+                </li>
+                </ul>
+            </nav>
+        </header>
+    </section>
+);
+
+export default header;
