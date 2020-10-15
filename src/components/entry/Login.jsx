ba29c95503acef8d8a99c104a7c618b789ad67be
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-15 16:34:24 -0500

    vista Login

---
+++ src/components/entry/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import img from '../../assets/logo-imax.png';
+import { Link } from 'react-router-dom';
+
+
+const login = () => (
+    <section className="login">
+        <form className="logIn"> 
+            <img className="logo-imax" alt="logo"  src={img}/>
+            <div className="formulary">
+                <div className="box-user"> <i className="user-icon fas fa-user"/> <input className="emailLogin user" type="email" placeholder="Correo Electrónico" required/> </div>
+                <div className="box-user"> <i className="user-icon fas fa-lock"/> <input className="passwordLogin user" type="password" placeholder="Contraseña" required/>  </div>
+                <div className="reg_error_inner" id="reg_error_inner"/>
+                <button type="submit" className= "btn-login"> Ingresar </button>
+            </div>
+            <p className="p-Options">¿No tienes cuenta? <Link  to="/registro">Regístrate</Link> </p>
+        </form>
+    </section>
+    
+);
+
+export default login;
