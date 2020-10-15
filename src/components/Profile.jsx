f3cfe072d526113e1f6593751132c8abe0c4fea1
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-15 16:33:28 -0500

    componente profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Header from './Header';
+
+
+const profile = () => (
+    <section className="form-profile">
+        <Header/>
+        <div className ="box-body">
+            <div className="box-profile">
+                <p className="title">Perfil del usuario</p>
+                <div className="box-user"> <label>Nombre: </label> <input className="nombre" type="text"  required/>   </div>
+                <div className="box-user"> <label>Área: </label> 
+                    <select name="area" className="select-modal" required >
+                    <option value="emp">Evaluación y Monitoreo de Proyectos</option>
+                    <option value="ie">Informes Especiales</option>
+                    <option value="iad">Innovacion y analisis de data</option>
+                    </select>  
+                </div>  
+                <div className="box-user"> <label>Email: </label> <input className="user" type="email" required/>  </div>     
+                <div className="box-user"> <label>Password: </label> <input className="user" type="password"  required/>  </div>         
+                <div className="box-btn">
+                <button className= "btn-guardar"> Guardar</button>
+                <button className= "btn-cancelar"> Cancelar </button>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+);
+
+export default profile;
