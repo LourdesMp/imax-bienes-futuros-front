2782dbd5a8763779dce027f66452107da25190e2
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-15 16:33:15 -0500

    componente New Project

---
+++ src/components/emp/NewProject.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from '../Header';
+
+
+
+const newProject = () => (
+    <section className="form-newProject">
+        <Header/>
+        <div class ="box-body">
+            <div class="box-newProject">
+                <p class="title">Datos del Proyecto</p>
+                <div class="box-user"> <label>Nombre: </label> <input class="nombre" type="text"  required/>   </div>
+                <div class="box-user"> <label>Promotor: </label> <input class="user" type="text" required/>  </div>  
+                <div class="box-user"> <label>Banco: </label> 
+                    <select name="area" className="select-modal" required >
+                    <option value="bcp">Banco de Credito del Perú</option>
+                    <option value="bbva">BBVA-Banco Continental</option>
+                    <option value="bp">Banco Pichincha</option>
+                    </select>  
+                </div>     
+                <div class="box-btn">
+                <button class= "btn-guardar"> Guardar</button>
+                <button class= "btn-cancelar"> Cancelar </button>
+                </div>
+            </div>
+        </div>
+   
+    </section>
+    
+);
+
+export default newProject;
