e386ec8e2f96ab9307fa198f1c875419bdedf3a0
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-16 13:04:22 -0500

    implementando metodo de login
    mediante firebase se usó el metodo SignIn para verificar si existe usuarios en la BD

--- src/components/entry/Login.jsx
+++ src/components/entry/Login.jsx
@@ -1,26 +1,69 @@
-import React from 'react';
+import React, {useState} from 'react';
 import img from '../../assets/logo-imax.png';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import 'firebase/auth';
+import { useFirebaseApp} from 'reactfire';
+
 
 
 const Login = () => {
+
+    const history = useHistory();
+    
+    const [emailLogin, setEmailLogin] = useState ('');
+
+    const [passwordLogin, setPasswordLogin ] = useState ('');
+    
+    const [error, setError] = useState({
+      email: false,
+      password: false,
+      message: '',
+    });
+
+    const firebase = useFirebaseApp(); 
+
+    //metodo para enviar a firebase el valor de los inputs y los valide
+    const sendLogin = () => {
+      const notValidEmail = emailLogin.trim() === '';
+      const notValidPassword = passwordLogin.trim() === '';
+      if (notValidEmail || notValidPassword ) {
+        if (notValidEmail) setError((prevState) => ({ ...prevState, emailLogin: true }));
+        else setError((prevState) => ({ ...prevState, emailLogin: false }));
+        if (notValidPassword) setError((prevState) => ({ ...prevState, passwordLogin: true }));
+        else setError((prevState) => ({ ...prevState, passwordLogin: false }));
+      } else {
+         firebase.auth().signInWithEmailAndPassword(emailLogin,passwordLogin)
+     .then(() => {
+      console.log('se ingresó Login');
+      history.push('/home');
+     }).catch((error)=>{
+      setError((prevState) => ({ ...prevState, message: error }));
+    });
+
+      }
+
+
+    
+       
+    }
   
     return (
     <section className="login">
-        <form className="logIn"> 
+        <div className="logIn"> 
             <img className="logo-imax" alt="logo"  src={img}/>
             <div className="formulary" >
-                <div className="box-user"> <i className="user-icon fas fa-user"/> 
-                <input className="emailLogin user" type="email" 
-                placeholder="Correo Electrónico"/> </div>
-                <div className="box-user"> <i className="user-icon fas fa-lock"/> 
-                <input className="passwordLogin user" type="password"
-                 placeholder="Contraseña"  />  </div>
-                <div className="reg_error_inner" id="reg_error_inner"/>
-                <button type="submit"className= "btn-login"> Ingresar </button>
+                <div className={error.emailLogin ? 'box-user box-error' : 'box-user'}> <i className="user-icon fas fa-user"/> 
+                <input className= {error.emailLogin ? 'email user errors' : 'email user'} type="email" 
+                placeholder={error.emailLogin ? 'Campo requerido' : 'Ingrese el email'} onChange={ (e)=> setEmailLogin(e.target.value)} /> </div>
+                <div className={error.passwordLogin ? 'box-user box-error' : 'box-user'}> <i className="user-icon fas fa-lock"/> 
+                <input className= {error.emailLogin ? 'password user errors' : 'password user'} type="password"
+                placeholder={error.passwordLogin ? 'Campo requerido' : 'Ingrese la contraseña'} onChange={(e) => setPasswordLogin(e.target.value)}  />  </div>
+                { error.message && <span>{error.message}</span> }
+                <button type="submit"className= "btn-login" onClick={sendLogin}> Ingresar </button>
             </div>
             <p className="p-Options">¿No tienes cuenta? <Link  to="/registro">Regístrate</Link> </p>
-        </form>
+        </div>
     </section>
     
 )};
