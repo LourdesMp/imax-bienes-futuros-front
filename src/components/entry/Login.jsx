fe0483266def842d508422a3d8b154d66e56a6ea
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-15 23:22:30 -0500

    editando form login

--- src/components/entry/Login.jsx
+++ src/components/entry/Login.jsx
@@ -3,20 +3,26 @@ import img from '../../assets/logo-imax.png';
 import { Link } from 'react-router-dom';
 
 
-const login = () => (
+const Login = () => {
+  
+    return (
     <section className="login">
         <form className="logIn"> 
             <img className="logo-imax" alt="logo"  src={img}/>
-            <div className="formulary">
-                <div className="box-user"> <i className="user-icon fas fa-user"/> <input className="emailLogin user" type="email" placeholder="Correo Electrónico" required/> </div>
-                <div className="box-user"> <i className="user-icon fas fa-lock"/> <input className="passwordLogin user" type="password" placeholder="Contraseña" required/>  </div>
+            <div className="formulary" >
+                <div className="box-user"> <i className="user-icon fas fa-user"/> 
+                <input className="emailLogin user" type="email" 
+                placeholder="Correo Electrónico"/> </div>
+                <div className="box-user"> <i className="user-icon fas fa-lock"/> 
+                <input className="passwordLogin user" type="password"
+                 placeholder="Contraseña"  />  </div>
                 <div className="reg_error_inner" id="reg_error_inner"/>
-                <button type="submit" className= "btn-login"> Ingresar </button>
+                <button type="submit"className= "btn-login"> Ingresar </button>
             </div>
             <p className="p-Options">¿No tienes cuenta? <Link  to="/registro">Regístrate</Link> </p>
         </form>
     </section>
     
-);
+)};
 
-export default login;
+export default Login;
