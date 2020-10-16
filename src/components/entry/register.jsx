a69ee429bbf27b26574662f2b3416e3dca350c8d
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-15 23:22:04 -0500

    eestableciendo conexion a firebase

--- src/components/entry/register.jsx
+++ src/components/entry/register.jsx
@@ -1,21 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
 import img from '../../assets/logo-imax.png';
+import { useHistory } from 'react-router-dom';
+import 'firebase/auth';
+import { useFirebaseApp} from 'reactfire';
 
 
-const register = () => (
+const Register = () => {
+    const history = useHistory();
+    
+    const [email, setEmail] = useState ('');
+
+    const [password, setPassword ] = useState ('');
+    
+    const [error, setError] = useState({
+      email: false,
+      password: false,
+      message: '',
+    });
+
+    const firebase = useFirebaseApp(); 
+
+    //metodo para capturar el valor de los inputs
+    const submit = () => {
+      const exRegEmail = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+      const notValidEmail = email.trim() === '';
+      const notValidPassword = password.trim() === '';
+      if (notValidEmail || notValidPassword || exRegEmail) {
+        if (notValidEmail) setError((prevState) => ({ ...prevState, email: true }));
+        else setError((prevState) => ({ ...prevState, email: false }));
+        if (notValidPassword) setError((prevState) => ({ ...prevState, password: true }));
+        else setError((prevState) => ({ ...prevState, password: false }));
+        if (exRegEmail) setError((prevState) => ({ ...prevState, email: true }));
+        else setError((prevState) => ({ ...prevState, email: false }));
+      } else {
+         firebase.auth().createUserWithEmailAndPassword(email,password)
+     .then(() => {
+      console.log('se ingresó');
+      history.push('/home');
+     }).catch((error)=>{
+      setError((prevState) => ({ ...prevState, message: error }));
+    });
+
+      }
+
+
+    
+       
+    }
+
+
+  return(
     <div className="form-register">
-       <form className="box-register"> 
+       <div className="box-register"> 
         <img className="logo-imax" src={img} alt="logo"  />
         <div className="formulary">
-          <div className="box-user"> <i className="user-icon fas fa-user"/> <input className="emailRegister user" type="email" placeholder="Correo Electrónico" required/>   </div>
-          <div className="box-user"> <i className="user-icon fas fa-lock"/> <input className="passwordRegister user" type="password" placeholder="Contraseña" required/>  </div>  
-          <div className="box-user"> <i className="user-icon fas fa-lock"/> <input className="passwordRegisterC user" type="password" placeholder="Confirmar contraseña" required/>  </div>       
-          <div className="reg_error_inner" id="reg_error_inner"></div>
-          <button type="submit" className= "btn-register"> Registrar </button>
+            <div className={error.email ? 'box-user box-error' : 'box-user'}> <i className="user-icon fas fa-user"/> 
+            <input className= {error.email ? 'email user errors' : 'email user'} type="email" 
+            placeholder={error.email ? 'Campo requerido' : 'Ingrese el email'} onChange={ (e)=> setEmail(e.target.value)} /> </div>
+            <div className={error.password ? 'box-user box-error' : 'box-user'}> <i className="user-icon fas fa-lock"/> 
+            <input className= {error.email ? 'password user errors' : 'password user'} type="password"
+              placeholder={error.password ? 'Campo requerido' : 'Ingrese la contraseña'} onChange={(e) => setPassword(e.target.value)}  />  </div>
+            { error.message && <span>{error.message}</span> }
+            <button type="submit" onClick={submit} className= "btn-register"> Registrar</button>
         </div>
-       </form>
+       </div>
     </div>
     
-);
+)};
 
-export default register;
+export default Register;
