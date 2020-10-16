e161dc21528c7e8c8fdbd17d8d5d1ca9a3d7a9eb
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-15 23:23:17 -0500

    jdk firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+
+
+
+export default {
+  apiKey: "AIzaSyBWmz14E95aGQKH6f_lbElFbrflbcSe5po",
+  authDomain: "bienesfuturos-291714.firebaseapp.com",
+  // databaseURL: "https://bienesfuturos-291714.firebaseio.com",
+  projectId: "bienesfuturos-291714",
+  storageBucket: "bienesfuturos-291714.appspot.com",
+  messagingSenderId: "857700584245",
+  appId: "1:857700584245:web:696cedadbc52d62a96da7c",
+  measurementId: "G-371B0KJDSB"
+};
