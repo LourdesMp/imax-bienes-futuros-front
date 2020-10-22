8807c70b51227ba40ae2e8b9e76e8cd9bd43e774
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-21 19:48:34 -0500

    rutas del home

---
+++ src/routers/DashboardRouters.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Switch, Route} from 'react-router-dom';
+
+import Home from '../components/Home';
+import Profile from '../components/Profile';
+import ListProjects from '../components/emp/ListProjects';
+import NewProject from '../components/emp/NewProject';
+import Tasacion from '../components/emp/Tasacion'
+import NotFound from '../components/NotFound';
+
+
+//función que engloba las rutas que estaran dentro de la vista home
+
+export const DashboardRouters = () => {
+    return (
+        <div className="dash__main">
+            <div className="dash__box-container">
+                <Switch>
+                    <Route exact path="/home" component={ Home }/>
+                    <Route exact path="/home/perfil" component={ Profile }/>
+                    <Route exact path="/home/proyectos" component={ ListProjects }/>
+                    <Route exact path="/home/nuevo" component={ NewProject }/>
+                    <Route exact path="/home/tasacion" component={ Tasacion }/>
+                    <Route exact path="/notfound" component={ NotFound }/>
+                </Switch>
+            </div>
+
+        </div>
+    )
+}
