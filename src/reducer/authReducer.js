e1595739c44c577031ff841ae60822cf99216bcf
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-21 19:47:09 -0500

    authReducer- inform de los usuarios

--- src/reducer/authReducer.js
+++ src/reducer/authReducer.js
@@ -1,5 +1,6 @@
 import { types } from '../types/types';
 
+//funcion que recibe el estado del usuario, y depende de ello retorna informacion 
 export const authReducer = (state= {}, action) => {
 switch (action.type) {
     case types.login:
