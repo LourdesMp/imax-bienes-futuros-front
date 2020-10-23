733a535d5306fbebc754f248120959ef30b5a4ae
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-23 17:36:29 -0500

    peticion API getTasaciones

---
+++ src/controller/tasaciones.js
@@ -0,0 +1,18 @@
+
+// peticion get de los proyectos a la database
+export const getTasaciones = () =>
+  fetch('http://localhost:3000/api/v1/tasaciones/listTasaciones', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    //   Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+    },
+  }).then((resp) => {
+    if (resp.status === 200) {
+      return resp.json();
+    } if (resp.status === 401) {
+      return new Error('No hay cabecera de autenticación');
+    } if (resp.status === 403) {
+      return new Error('No tiene permiso');
+    }
+  });
