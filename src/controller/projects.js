97f159baca875473b431f28d773b84352f26f319
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-26 17:05:58 -0500

    peticion post

--- src/controller/projects.js
+++ src/controller/projects.js
@@ -39,39 +39,39 @@ export const getProjects = () =>
   }
 });
 
-export const deleteProduct = (id) => fetch(`http://localhost:8000/api/v1/proyectos/list/${id}`, {
-  method: 'DELETE',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-}).then((resp) => {
-  if (resp.status === 200) {
-    return resp.json();
-  } if (resp.status === 401) {
-    return new Error('No hay cabecera de autenticación');
-  } if (resp.status === 403) {
-    return new Error('No tiene permiso para realizar la acción');
-  } if (resp.status === 404) {
-    return new Error('Proyecto no encontrado');
-  }
-});
+// export const deleteProduct = (id) => fetch(`http://localhost:8000/api/v1/proyectos/list/${id}`, {
+//   method: 'DELETE',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// }).then((resp) => {
+//   if (resp.status === 200) {
+//     return resp.json();
+//   } if (resp.status === 401) {
+//     return new Error('No hay cabecera de autenticación');
+//   } if (resp.status === 403) {
+//     return new Error('No tiene permiso para realizar la acción');
+//   } if (resp.status === 404) {
+//     return new Error('Proyecto no encontrado');
+//   }
+// });
 
-export const putProduct = (product, id) => fetch(`http://localhost:8000/api/v1/proyectos/list/${id}`, {
-  method: 'PUT',
-  body: JSON.stringify(product),
-  headers: {
-    'Content-Type': 'application/json',
-  },
-}).then((resp) => {
-  if (resp.status === 200) {
-    return resp.json();
-  } if (resp.status === 400) {
-    return new Error('No hay ningún campo a modificar');
-  } if (resp.status === 401) {
-    return new Error('No hay cabecera de autenticación');
-  } if (resp.status === 403) {
-    return new Error('No tiene permiso para realizar la acción');
-  } if (resp.status === 404) {
-    return new Error('Producto no encontrado');
-  }
-});
+// export const putProduct = (product, id) => fetch(`http://localhost:8000/api/v1/proyectos/list/${id}`, {
+//   method: 'PUT',
+//   body: JSON.stringify(product),
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// }).then((resp) => {
+//   if (resp.status === 200) {
+//     return resp.json();
+//   } if (resp.status === 400) {
+//     return new Error('No hay ningún campo a modificar');
+//   } if (resp.status === 401) {
+//     return new Error('No hay cabecera de autenticación');
+//   } if (resp.status === 403) {
+//     return new Error('No tiene permiso para realizar la acción');
+//   } if (resp.status === 404) {
+//     return new Error('Producto no encontrado');
+//   }
+// });
--- src/controller/tasaciones.js
+++ src/controller/tasaciones.js
@@ -16,3 +16,24 @@ export const getTasaciones = () =>
       return new Error('No tiene permiso');
     }
   });
+
+  export const postTasacion = (tasacion) => fetch('http://localhost:8000/api/v1/tasaciones/newTasacion', {
+    method: 'POST',
+    body: JSON.stringify(tasacion),
+    headers: {
+      'Content-Type': 'application/json',
+      // Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+    },
+  }).then((resp) => {
+    if (resp.status === 200) {
+      return resp.json();
+    } if (resp.status === 400) {
+      return new Error('Faltan el nombre del proyecto');
+    } if (resp.status === 401) {
+      return new Error('No hay cabecera de autenticación');
+    } if (resp.status === 403) {
+      return new Error('No tiene permiso para realizar esta acción');
+    } if (resp.status === 404) {
+      return new Error('No existe un producto con ese Id');
+    }
+  });
