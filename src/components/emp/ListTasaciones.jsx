d9059bd441f22513adb12a6bae52f1342a72a113
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-26 17:03:36 -0500

    creand estado para tasaciones

--- src/components/emp/ListTasaciones.jsx
+++ src/components/emp/ListTasaciones.jsx
@@ -10,24 +10,24 @@ const ListTasaciones = () => {
     const [tasacion, setTasacion] = useState ({
         DataTasaciones: [],
         tasacionData : {
-            IdTasacion: '',
-            NombreCliente: '',
-            DniCliente: '',
-            NombreConyugue: '',
-            DniConyugue: '',
-            TipodeCambio: '',
-            IdProyecto: '',
-            NombreProyecto: '',
-            DireccionProyecto: '',
-            Promotor: '',
-            Banco: '',
-            Departamento: '',
-            Estacionamiento: '',
-            ValorVenta: '',
-            Moneda: '',
-            ValorMetroCuadrado: '',
-            ValorComercial: '',
-            PorcentajeRevision: ''
+            idTasacion: '',
+            nombreCliente: '',
+            dniCliente: '',
+            nombreConyugue: '',
+            dniConyugue: '',
+            tipodeCambio: '',
+            nombreProyecto: '',
+            direccionProyecto: '',
+            promotor: '',
+            banco: '',
+            departamento: '',
+            estacionamiento: '',
+            deposito:'',
+            valorVenta: '',
+            moneda: '',
+            valorMetroCuadrado: '',
+            valorComercial: '',
+            porcentajeRevision: ''
         }
     });
 
@@ -35,7 +35,9 @@ const ListTasaciones = () => {
         getTasaciones().then((resp) => setTasacion ((prevState) => ({
             ...prevState,
             DataTasaciones: resp.DataTasaciones
+            
         })));
+        console.log(setTasacion)
     }, []);
 
 
@@ -52,7 +54,7 @@ const ListTasaciones = () => {
                         <div className="box-first">
                             <label className="title-tasacion">Buscar por Tasacion: </label> 
                             <div className="search-input"> <input className="nombre" type="text" placeholder="Nombres y apellidos" /> 
-                            <input className="dni" placeholder="DNI"type="text" /> </div>
+                             </div>
                         </div>
                     </div> 
                     
@@ -60,10 +62,24 @@ const ListTasaciones = () => {
                     <table className="table-list">
                         <thead>
                             <tr>
+                                <th>Id Tasacion</th>
+                                <th>Nombre del Cliente</th>
+                                <th>Dni del Cliente</th>
+                                <th>Nombre del Conyugue</th>
+                                <th>Dni del Conyugue</th>
+                                <th>Tipo de Cambio</th>
                                 <th>Nombre del Proyecto</th>
                                 <th>Dirección</th>
                                 <th>Promotor</th>
                                 <th>Banco</th>
+                                <th>Departamento</th>
+                                <th>Estacionamiento</th>
+                                <th>Deposito</th>
+                                <th>Valor de Venta</th>
+                                <th>Moneda</th>
+                                <th>Valor Metro Cuadrado</th>
+                                <th>Valor Comercial</th>
+                                <th>Porcentaje Revision</th>
                                 <th>Matriz de Tasaciones</th>
                                 <th>Formato</th>
                             </tr>
@@ -74,11 +90,25 @@ const ListTasaciones = () => {
                             tasacion.DataTasaciones.length > 0
 
                             ? tasacion.DataTasaciones.map((element) => (
-                                <tr key={element.IdTasacion}>
-                                    <td>{element.NombreProyecto}</td>
-                                    <td>{element.DireccionProyecto}</td>
-                                    <td>{element.Promotor}</td>
-                                    <td>{element.Banco}</td>
+                                <tr key={element.idTasacion}>
+                                    <td>{element.idTasacion}</td>
+                                    <td>{element.nombreCliente}</td>
+                                    <td>{element.dniCliente}</td>
+                                    <td>{element.nombreConyugue}</td>
+                                    <td>{element.dniConyugue}</td>
+                                    <td>{element.tipodeCambio}</td>
+                                    <td>{element.nombreProyecto}</td>
+                                    <td>{element.direccionProyecto}</td>
+                                    <td>{element.promotor}</td>
+                                    <td>{element.banco}</td>
+                                    <td>{element.departamento}</td>
+                                    <td>{element.estacionamiento}</td>
+                                    <td>{element.deposito}</td>
+                                    <td>{element.valorVenta}</td>
+                                    <td>{element.moneda}</td>
+                                    <td>{element.valorMetroCuadrado}</td>
+                                    <td>{element.valorComercial}</td>
+                                    <td>{element.porcentajeRevision}</td>
                                     <td><i className="user-upload fas fa-upload"></i><i className="user-down fas fa-download"></i></td>
                                     <td><i className="user-upload fas fa-upload"></i><i className="user-down fas fa-download"></i><i className="user-remove far fa-trash-alt"></i></td>
                                 </tr>
@@ -91,7 +121,7 @@ const ListTasaciones = () => {
                         </tbody>
                     </table>
                     
-                         <Link className="btn-newTasacion"  to="/home/tasacion">Nueva Tasación</Link>
+                         <Link className="btn-newTasacion"  to="/home/newtasacion">Nueva Tasación</Link>
                     
                 </div> 
         
