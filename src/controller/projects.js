de2050579ce6acc3121f8e4d60c381d6611523af
LourdesMp <lourdes.merino@imax.com.pe>
2020-11-06 20:38:28 -0500

    agregando peticion a la API del Id

--- src/controller/projects.js
+++ src/controller/projects.js
@@ -18,6 +18,23 @@ export const getProjects = () =>
     }
   });
 
+  export const getLastId = () =>
+  fetch('http://localhost:8000/api/v1/proyectos/getLastId', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    //   Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+    },
+  }).then((resp) => {
+    if (resp.status === 200) {
+      return resp.json();
+    } if (resp.status === 401) {
+      return new Error('No hay cabecera de autenticación');
+    } if (resp.status === 403) {
+      return new Error('No tiene permiso');
+    }
+  });
+
 
  
 
