23b5e30485fee9909a11a2fcd791fcc9ae81ec29
LourdesMp <lourdes.merino@imax.com.pe>
2020-11-23 12:58:22 -0500

    manejo de errores con redux

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect}from 'react';
 import img from '../../assets/logo-imax.png';
 import { Link } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm';
@@ -15,6 +15,32 @@ const Login = () => {
   const {msgError} = useSelector ( state => state.uiReducer);
   console.log(msgError)
 
+  const [msgErr, setMsgErr] = useState(null);
+
+  const {
+    isLoading,
+    isSuccess,
+    isError,
+    data,
+    error
+} = useSelector ( state => state.authReducer.user);
+
+  useEffect(() => {
+    if(error){
+      switch (error.code) {
+        case 'auth/wrong-password':
+          setMsgErr('Contraseña incorrecta');
+          break;
+        case'auth/user-not-found':
+          setMsgErr('Email incorrecto');
+          break;
+        default:
+          setMsgErr('Email o contraseña incorrecta')
+          break;
+      }
+    }
+  }, [error])
+
   //uso del hooks que se creo, para determinar cuando se está llenando
   const [formValues, handleInputChange] = useForm ({
     emailLogin:'',
@@ -29,7 +55,8 @@ const Login = () => {
     e.preventDefault();
     if (isFormValidLogin()){
       console.log(emailLogin, passwordLogin)
-      dispatch ( startLoginEmailPassword  (emailLogin, passwordLogin)) 
+      dispatch( startLoginEmailPassword(emailLogin, passwordLogin)) 
+      
     }
  
   };
@@ -37,11 +64,11 @@ const Login = () => {
   //funcion que identifica los errores para que se puedan mostrar 
   const isFormValidLogin = () => {
     if (emailLogin.trim() === '') {
-      dispatch ( setError ('ingrese su email'));
+      dispatch( setError('ingrese su email'));
       return false;
     }
     else if (passwordLogin.trim() === '') {
-      dispatch ( setError ('ingrese su password'));
+      dispatch ( setError('ingrese su password'));
       return false;
     }
 
@@ -61,7 +88,7 @@ const Login = () => {
                 <div className={msgError ? 'box-user box-error' : 'box-user'}> <i className="user-icon fas fa-lock"/> 
                 <input className= {msgError ? 'password user errors' : 'password user'} type="password" name="passwordLogin"
                 placeholder={msgError ? 'Campo requerido' : 'Ingrese la contraseña'} onChange={handleInputChange} value= {passwordLogin}  />  </div>
-                { msgError && (<span> {msgError}</span>) }
+                { msgErr && (<span> {msgErr}</span>) }
                 <button type="submit"className= "btn-login" > Ingresar </button>
             </div>
             <p className="p-Options">¿No tienes cuenta? <Link  to="/register">Regístrate</Link> </p>
